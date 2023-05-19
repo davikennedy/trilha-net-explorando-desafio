429c749f84f4243742496ce3b49c914a89964019
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-05-18 23:35:59 -0300

    Refatorando cmétodo CadastrarHospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,18 +15,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (Suite.Capacidade >= hospedes.Count)
+            if (hospedes.Count > Suite.Capacidade)
             {
-                Hospedes = hospedes;
+                throw new ArgumentException("Capacidade máxima atingida. Não é possível adicionar mais hóspedes.");
+
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-                throw new Exception("Capacidade máxima atingida. Não é possível adicionar mais hóspedes.");
-            }
+
+            Hospedes = hospedes;
         }
 
         public void CadastrarSuite(Suite suite)
@@ -36,20 +31,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor *= 0.9m;
